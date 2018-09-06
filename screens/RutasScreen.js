57e811ce0a9c22c0521d9be2a243bb23bd9d1532
Ismael Sagredo <sagredo.ismael@live.com>
2018-09-06 23:30:57 +0200

    Add listView

--- screens/RutasScreen.js
+++ screens/RutasScreen.js
@@ -5,8 +5,10 @@ import {
   Platform,
   ScrollView,
   StyleSheet,
+  ListView,
   Text,
   TouchableOpacity,
+  TouchableHighlight,
   View,
 } from 'react-native';
 
@@ -15,11 +17,55 @@ export default class RutasScreen extends React.Component {
     title: 'Rutas cerca de mí',
   };
 
+  constructor(){
+    super()
+    var ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+    this.state = {
+      itemDataSource: ds
+    }
+    this.renderRow = this.renderRow.bind(this);
+    this.pressRow = this.pressRow.bind(this);
+  }
+
+  componentWillMount(){
+    this.getItems();
+  }
+
+  componentDidMount(){
+    this.getItems();
+  }
+
+  getItems(){
+    let items = [{title:'Item uno'}, {title:'Item dos coño'}];
+
+    this.setState({
+        itemDataSource : this.state.itemDataSource.cloneWithRows(items)
+    })
+  }
+
+  renderRow(item){
+    return (
+      <TouchableHighlight onPress = {() => {
+        this.pressRow(item)
+      }}>
+        <View style={styles.container}>
+          <Text> El titulo de lo que has seleccionado es: {item.title} </Text>
+        </View>
+      </TouchableHighlight>
+    );
+  }
+
+  pressRow(){
+    console.log(item)
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <ExpoLinksView />
-
+        <ListView
+          dataSource = {this.state.itemDataSource}
+          renderRow = {this.renderRow}
+        />
       </View>
     );
   }
